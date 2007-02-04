0fbbf71f673bfc85e621e67460dccd0f3d913149
hansp <hansp>
2007-02-04 08:27:17 +0000

    Implement functions that can insert elements in a connected pipeline. Implement a function that replaces one element with another, taking over the former's connectivity.

--- flow/flow-util.h
+++ flow/flow-util.h
@@ -35,28 +35,52 @@ G_BEGIN_DECLS
 
 typedef void (*FlowNotifyFunc) (gpointer user_data);
 
-void         flow_connect_simplex__simplex        (FlowSimplexElement *output_simplex,
-                                                   FlowSimplexElement *input_simplex);
-void         flow_connect_duplex__duplex          (FlowDuplexElement *downstream_duplex,
-                                                   FlowDuplexElement *upstream_duplex);
-void         flow_connect_simplex_simplex__duplex (FlowSimplexElement *downstream_simplex_output,
-                                                   FlowSimplexElement *downstream_simplex_input,
-                                                   FlowDuplexElement  *upstream_duplex);
-void         flow_connect_duplex__simplex_simplex (FlowDuplexElement  *downstream_duplex,
-                                                   FlowSimplexElement *upstream_simplex_output,
-                                                   FlowSimplexElement *upstream_simplex_input);
-void         flow_disconnect_element              (FlowElement *element);
+void         flow_connect_simplex__simplex         (FlowSimplexElement *output_simplex,
+                                                    FlowSimplexElement *input_simplex);
+void         flow_connect_duplex__duplex           (FlowDuplexElement *downstream_duplex,
+                                                    FlowDuplexElement *upstream_duplex);
+void         flow_connect_simplex_simplex__duplex  (FlowSimplexElement *downstream_simplex_output,
+                                                    FlowSimplexElement *downstream_simplex_input,
+                                                    FlowDuplexElement  *upstream_duplex);
+void         flow_connect_duplex__simplex_simplex  (FlowDuplexElement  *downstream_duplex,
+                                                    FlowSimplexElement *upstream_simplex_output,
+                                                    FlowSimplexElement *upstream_simplex_input);
 
-FlowPacket  *flow_create_simple_event_packet      (const gchar *domain, gint code);
-gboolean     flow_handle_universal_events         (FlowElement *element, FlowPacket *packet);
-gpointer     flow_read_object_from_shunt          (FlowShunt *shunt);
+void         flow_insert_Isimplex__simplex         (FlowSimplexElement *output_simplex_inserted,
+                                                    FlowSimplexElement *input_simplex);
+void         flow_insert_simplex__Isimplex         (FlowSimplexElement *output_simplex,
+                                                    FlowSimplexElement *input_simplex_inserted);
+void         flow_insert_Iduplex__duplex           (FlowDuplexElement *downstream_duplex_inserted,
+                                                    FlowDuplexElement *upstream_duplex);
+void         flow_insert_duplex__Iduplex           (FlowDuplexElement *downstream_duplex,
+                                                    FlowDuplexElement *upstream_duplex_inserted);
+void         flow_insert_Isimplex_Isimplex__duplex (FlowSimplexElement *downstream_simplex_output_inserted,
+                                                    FlowSimplexElement *downstream_simplex_input_inserted,
+                                                    FlowDuplexElement *upstream_duplex);
+void         flow_insert_duplex__Isimplex_Isimplex (FlowDuplexElement *downstream_duplex,
+                                                    FlowSimplexElement *upstream_simplex_output_inserted,
+                                                    FlowSimplexElement *upstream_simplex_input_inserted);
+void         flow_insert_Iduplex__simplex_simplex  (FlowDuplexElement *downstream_duplex_inserted,
+                                                    FlowSimplexElement *upstream_simplex_output,
+                                                    FlowSimplexElement *upstream_simplex_input);
+void         flow_insert_simplex_simplex__Iduplex  (FlowSimplexElement *downstream_simplex_output,
+                                                    FlowSimplexElement *downstream_simplex_input,
+                                                    FlowDuplexElement *upstream_duplex_inserted);
 
-gint         flow_g_ptr_array_find                (GPtrArray *array, gpointer data);
-gboolean     flow_g_ptr_array_remove_sparse       (GPtrArray *array, gpointer data);
-void         flow_g_ptr_array_add_sparse          (GPtrArray *array, gpointer data);
-guint        flow_g_ptr_array_compress            (GPtrArray *array);
+void         flow_disconnect_element               (FlowElement *element);
 
-gchar       *flow_strerror                        (gint errnum);
+void         flow_replace_element                  (FlowElement *element, FlowElement *replacement);
+
+FlowPacket  *flow_create_simple_event_packet       (const gchar *domain, gint code);
+gboolean     flow_handle_universal_events          (FlowElement *element, FlowPacket *packet);
+gpointer     flow_read_object_from_shunt           (FlowShunt *shunt);
+
+gint         flow_g_ptr_array_find                 (GPtrArray *array, gpointer data);
+gboolean     flow_g_ptr_array_remove_sparse        (GPtrArray *array, gpointer data);
+void         flow_g_ptr_array_add_sparse           (GPtrArray *array, gpointer data);
+guint        flow_g_ptr_array_compress             (GPtrArray *array);
+
+gchar       *flow_strerror                         (gint errnum);
 
 G_END_DECLS
 
