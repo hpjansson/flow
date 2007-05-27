ee10a5a94d269bdc00223b10ab987ef4e4bd6493
hansp <hansp>
2007-05-27 01:32:13 +0000

    Lower priority for valgrind stress tests.

--- test/stress-valgrind.sh
+++ test/stress-valgrind.sh
@@ -26,7 +26,7 @@ while true; do
   echo -n -e "\rIterations: $m"
 
   for i in $(cat tests.list); do
-    valgrind -q --trace-children=yes --track-fds=yes --time-stamp=yes --num-callers=32 .libs/lt-$i -n >out 2>&1
+    nice -10 valgrind -q --trace-children=yes --track-fds=yes --time-stamp=yes --num-callers=32 .libs/lt-$i -n >out 2>&1
 
     if grep -L 'Thread' out >/dev/null 2>&1; then
       echo -e '\nFailure: ' $i
