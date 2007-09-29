07cee226d18b5ea42614b923862c01961eecb0cf
hansp <hansp>
2007-09-29 04:45:52 +0000

    Run gdb through an expect script, so we can recover when it gets messed up.

--- test/stress-gdb.sh
+++ test/stress-gdb.sh
@@ -13,9 +13,14 @@ export MALLOC_PERTURB_=a
 let n=0
 let m=0
 
-echo 'run -a'     > .gdb-commands
-echo 't a a bt'  >> .gdb-commands
-echo 'quit'      >> .gdb-commands
+# Check prerequisites
+
+for util in gdb expect dos2unix; do
+  if ! which $util >/dev/null 2>&1; then
+    echo "The '$util' utility was not found in your path. Aborting."
+    exit 1
+  fi
+done
 
 # Make sure binaries are created in .libs
 
@@ -31,9 +36,46 @@ while true; do
   echo -n -e "\rIterations: $m"
 
   for i in $(cat tests.list); do
-    gdb -q -batch -x .gdb-commands .libs/lt-$i >out 2>&1
+    cat <<EOF >.expect-commands
+proc send {ignore arg} {
+  sleep .1
+  exp_send -- \$arg
+}
 
-    if ! grep -L 'passed$' out >/dev/null 2>&1; then
+proc sendctrlc {} {
+  send -- ""
+}
+
+set timeout 120
+spawn gdb -n -q .libs/lt-$i
+match_max 100000
+expect -exact "(gdb) "
+send -- "set pagination off\r"
+expect -exact "(gdb) "
+send -- "run -n -v\r"
+expect {
+  timeout {
+    sendctrlc
+    exp_continue
+  }
+  "(gdb) "
+}
+send -- "t a a bt\r"
+expect -exact "(gdb) "
+send -- "quit\r"
+expect {
+  "(y or n) " {
+    send -- "y\r"
+    exp_continue
+  }
+  eof
+}
+EOF
+
+    # We need the dos2unix stage for grep's sake.
+    expect -f .expect-commands 2>&1 | dos2unix > out
+
+    if ! grep -L 'passed' out >/dev/null 2>&1; then
       echo -e '\nFailure: ' $i
       mv out stress-gdb-failure-$n.log
       let n=$n+1
@@ -45,4 +87,4 @@ while true; do
   sleep 1
 done 2>&1
 
-rm .gdb-commands
+rm .expect-commands
