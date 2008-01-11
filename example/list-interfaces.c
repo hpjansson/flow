0909bada2cdd1549295a414ff169354ed6d038d7
Hans Petter Jansson <hpj@cl.no>
2008-01-10 22:01:26 -0600

    Implement the first example: list-interfaces.

---
+++ example/list-interfaces.c
@@ -0,0 +1,80 @@
+/* -*- Mode: C; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+/* list-interfaces.c - List active IP interfaces on system.
+ *
+ * Copyright (C) 2008 Hans Petter Jansson
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Authors: Hans Petter Jansson <hpj@copyleft.no>
+ */
+
+#define __USE_GNU 1
+
+#include "config.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <glib/gprintf.h>
+
+#include <flow/flow.h>
+
+gint
+main (gint argc, gchar *argv [])
+{
+  GList      *interface_list;
+  FlowIPAddr *internet_interface;
+  GList      *l;
+
+  g_type_init ();
+
+  internet_interface = flow_ip_addr_get_internet_interface ();
+  interface_list = flow_ip_addr_get_interfaces ();
+
+  for (l = interface_list; l; l = g_list_next (l))
+  {
+    FlowIPAddr       *this_interface = l->data;
+    FlowIPAddrFamily *family;
+    gchar            *name;
+
+    name = flow_ip_addr_get_string (this_interface);
+
+    g_print ("%s\n", name);
+
+    g_free (name);
+    g_object_unref (this_interface);
+  }
+
+  if (internet_interface)
+  {
+    gchar *name = flow_ip_addr_get_string (internet_interface);
+    g_print ("---\n%s is the Internet interface.\n", name);
+    g_free (name);
+  }
+  else
+    g_print ("---\nNo Internet interface.\n");
+
+  g_list_free (interface_list);
+  if (internet_interface)
+    g_object_unref (internet_interface);
+  return 0;
+}
